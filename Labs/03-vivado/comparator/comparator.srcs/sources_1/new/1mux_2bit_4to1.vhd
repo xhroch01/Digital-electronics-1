363f72d620b25fa46aff7c3f5e2b3b47f7657f5e
xhroch01 <78789472+xhroch01@users.noreply.github.com>
2021-02-24 21:55:54 +0100

    xhroch01

---
+++ Labs/03-vivado/comparator/comparator.srcs/sources_1/new/1mux_2bit_4to1.vhd
@@ -0,0 +1,30 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+------------------------------------------------------------------------
+-- Entity declaration for 2-bit binary comparator
+------------------------------------------------------------------------
+entity 1mux_2bit_4to1 is
+    port(
+      a_i           : in  std_logic_vector(2 - 1 downto 0);
+	  b_i           : in  std_logic_vector(2 - 1 downto 0);
+	  c_i           : in  std_logic_vector(2 - 1 downto 0);
+	  d_i           : in  std_logic_vector(2 - 1 downto 0);
+	  sel_i         : in  std_logic_vector(2 - 1 downto 0);
+
+        -- COMPLETE ENTITY DECLARATION
+        f_o         : out std_logic_vector(2 - 1 downto 0)
+    );
+end entity 1mux_2bit_4to1;
+
+------------------------------------------------------------------------
+-- Architecture body for 2-bit binary comparator
+------------------------------------------------------------------------
+architecture Behavioral of 1mux_2bit_4to1 is
+begin
+    f_o <= a_i when (sel_i = "00") else
+           b_i when (sel_i = "01") else
+           c_i when (sel_i = "10") else
+           d_i;
+
+end architecture Behavioral;
